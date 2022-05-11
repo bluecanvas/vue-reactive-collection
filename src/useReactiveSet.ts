c177ef8609212a47d04ae676e883a750dd93ca4b
Andrew <44983823+andrewvasilchuk@users.noreply.github.com>
2022-05-11 16:35:17 +0300

    fix: props lost reactivity (#1)

--- src/useReactiveSet.ts
+++ src/useReactiveSet.ts
@@ -4,24 +4,18 @@ import type { SetConstructorArgument } from './types'
 import ReactiveSet from './ReactiveSet'
 
 export default function useReactiveSet<T>(values?: SetConstructorArgument<T>) {
-  const version = ref(1)
-
   function onMutate() {
-    version.value += 1
+    set.value = set.value
   }
 
   const inner = ref(new ReactiveSet<T>(onMutate, values))
 
   const set = computed<Set<T>>({
     get: () => {
-      // By using `version` we tell Vue that this property depends on it,
-      // so it gets re-evaluated whenever `version` changes
-      version.value
       return inner.value
     },
     set: (set: Set<T>) => {
       inner.value = new ReactiveSet(onMutate, set)
-      version.value += 1
     },
   })
 
